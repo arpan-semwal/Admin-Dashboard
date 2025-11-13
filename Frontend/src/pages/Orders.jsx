307a2d3fa07335cfe6ac815a32dc6a0e36a1f2ce
arpan-semwal <arpansemwal8@gmail.com>
2025-11-13 23:49:59 +0530

    userspage-data fetched and styled and orders page data displayed

--- Frontend/src/pages/Orders.jsx
+++ Frontend/src/pages/Orders.jsx
@@ -1,8 +1,31 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { fetchOrders } from '../api/orders'
 
 const Orders = () => {
+
+  const {data:ordersData , isLoading}  = useQuery({
+    queryKey:["orders"],
+    queryFn:fetchOrders
+  })
+
+
+
   return (
-    <div>Orders</div>
+    <div>
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : (
+        <div>
+          {ordersData?.carts?.map((order) => (
+            <div key={order.id}>
+              <h3>{order.total}</h3>
+             
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
 
